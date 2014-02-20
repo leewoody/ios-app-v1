3e94ff4eb5626b2224f053b53d9555a16bf51bed
Kevin Meyer <meyer.kevin@mytum.de>
2014-02-20 15:51:11 +0100

    added Settings. save settings to default preferences. ask at startup if no settings provided.

---
+++ Wallabag/WALSettings.h
@@ -0,0 +1,13 @@
+//
+//  WALSettings.h
+//  Wallabag
+//
+//  Created by Kevin Meyer on 20.02.14.
+//  Copyright (c) 2014 Wallabag. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WALSettings : NSObject
+
+@end
---
+++ Wallabag/WALSettings.m
@@ -0,0 +1,13 @@
+//
+//  WALSettings.m
+//  Wallabag
+//
+//  Created by Kevin Meyer on 20.02.14.
+//  Copyright (c) 2014 Wallabag. All rights reserved.
+//
+
+#import "WALSettings.h"
+
+@implementation WALSettings
+
+@end
