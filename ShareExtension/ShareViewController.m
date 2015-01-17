e176877fad7101c390e8e84034df562d9dc4479e
Kevin Meyer <meyer.kevin@mytum.de>
2015-01-17 14:07:37 +0100

    added some auto-close functionality, when link is successfully added.

--- ShareExtension/ShareViewController.m
+++ ShareExtension/ShareViewController.m
@@ -9,7 +9,7 @@
 #import "ShareViewController.h"
 #import "WALSettings.h"
 
-@interface ShareViewController ()
+@interface ShareViewController () <UIWebViewDelegate>
 @property (weak) IBOutlet UIWebView *webView;
 @property (strong) WALSettings *settings;
 - (IBAction)cancelPushed:(id)sender;
@@ -23,6 +23,8 @@
 }
 
 - (void)viewDidLoad {
+	self.webView.delegate = self;
+	
 	self.settings = [[WALSettings alloc] init];
 	[self.settings setWallabagURL:[NSURL URLWithString:@"https://example.com/"]];
 	
@@ -33,10 +35,30 @@
 		NSURLRequest *request = [NSURLRequest requestWithURL:[self.settings getURLToAddArticle:url]];
 		[self.webView loadRequest:request];
 	}];
-	
 }
 
 - (IBAction)cancelPushed:(id)sender {
 	[self.extensionContext cancelRequestWithError:nil];
 }
+
+
+#pragma mark - WebViewDelegate
+
+- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
+	NSLog(@"URL Request:\n\tMethod: %@\n\tURL: %@\n\tBody: %@\n\tPathExtension: %@", request.HTTPMethod, request.URL.absoluteString, request.HTTPBody, request.URL.pathExtension);
+	NSURL *url = request.URL;
+	
+	if ([url.query isEqualToString:@"view=home&closewin=true"]) {
+		self.title = @"Success";
+		NSLog(@"Success!");
+		[self.extensionContext completeRequestReturningItems:nil completionHandler:nil];
+	} else if (![url.pathExtension isEqualToString:@"php"]) {
+		NSError *error = [NSError errorWithDomain:@"wallabag-share" code:111 userInfo:@{NSLocalizedDescriptionKey: @"Error adding Link. Please try again"}];
+		NSLog(@"Couldn't add Link");
+		[self.extensionContext cancelRequestWithError:error];
+	}
+	return YES;
+}
+
+
 @end
