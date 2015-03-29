6812c8d56af41ef89f71bf3b9f44f590467bf0cc
Kevin Meyer <meyer.kevin@mytum.de>
2015-03-29 23:55:06 +0200

    added fastlane tools: screenshot + deliver

---
+++ snapshot.js
@@ -0,0 +1,38 @@
+#import "SnapshotHelper.js"
+
+var target = UIATarget.localTarget();
+var app = target.frontMostApp();
+var window = app.mainWindow();
+
+target.delay(3);
+target.frontMostApp().mainWindow().tableViews()[0].cells()[0].textFields()[0].setValue("http://localhost/");
+target.delay(1);
+target.frontMostApp().mainWindow().tableViews()[0].cells()[1].textFields()[0].setValue("3");
+target.delay(1);
+target.frontMostApp().mainWindow().tableViews()[0].cells()[2].textFields()[0].setValue("TRgHMwDCkJ9nv");
+target.delay(1);
+
+target.frontMostApp().navigationBar().rightButton().tap();
+target.delay(3);
+
+captureLocalizedScreenshot("0-Feed");
+
+target.frontMostApp().mainWindow().tableViews()[0].cells()[0].tap();
+target.delay(2);
+captureLocalizedScreenshot("1-Article1");
+
+target.frontMostApp().navigationBar().leftButton().tap();
+target.delay(1);
+
+target.frontMostApp().mainWindow().tableViews()[0].cells()[1].tap();
+target.delay(2);
+captureLocalizedScreenshot("2-Article2");
+
+target.frontMostApp().navigationBar().leftButton().tap();
+target.delay(1);
+
+target.frontMostApp().mainWindow().tableViews()[0].cells()[2].tap();
+target.delay(1);
+target.frontMostApp().toolbar().buttons()[1].tap();
+target.delay(2);
+captureLocalizedScreenshot("3-Article3-Dark");
